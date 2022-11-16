43b42ddc56fbb4ed791a7af3bfb91a2a3a69ac60
JacqueBP <66834787+JacqueBP@users.noreply.github.com>
2022-11-15 20:25:44 -0600

    configuracion.java

---
+++ src/main/java/uam/practica/uno/spring/configuracion/configuracion.java
@@ -0,0 +1,12 @@
+package uam.practica.uno.spring.configuracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uam.practica.uno.spring.PracticaUnoApplication;
+
+@SpringBootApplication
+public class configuracion {
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaUnoApplication.class, args);
+	}
+}
